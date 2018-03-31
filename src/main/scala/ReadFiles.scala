3515641eb63ed63583cdd9204e18d09133031c57
icemanx7 <richard.thomas36@gmail.com>
2018-03-31 13:25:51 +0200

    finished the file remover

--- src/main/scala/ReadFiles.scala
+++ src/main/scala/ReadFiles.scala
@@ -5,11 +5,7 @@ import scala.annotation.tailrec
 object ReadFiles {
 
   def main(args: Array[String]): Unit = {
-    println("Richard")
-    val files = getListOfFiles("/home/icemanx7/Documents")
-    println(makeAllFiles(files, List()))
-    println(reduceFoldersToList("/home/icemanx7/Documents").filter(x => x.getName.takeRight(3) == "pdf").length)
-    reduceFoldersToList("/home/icemanx7/Documents").filter(x => x.getName.takeRight(3) == "pdf").foreach(println)
+    initMove(args)
   }
 
   def getListOfFiles(dir: String): List[File] = {
@@ -19,6 +15,36 @@ object ReadFiles {
     } else {
       List[File]()
     }
+  }
+
+  def getCommandArgs(cArgs: Array[String]): Option[(String, String)] = {
+    Some(cArgs(0), cArgs(1))
+  }
+
+  def initMove(cArgs: Array[String]): Unit = {
+    val somePathAndType = getCommandArgs(cArgs)
+    somePathAndType match {
+      case None =>
+        println("Please input 2 path arguments.")
+        println("usage: scala Readfiles.scala <path> <file type>")
+      case Some(pathType) =>
+        val initList = reduceFoldersToList(pathType._1)
+        val filterList = filterByFileType(pathType._2, initList)
+        moveToSuppliedFolder(pathType._1, filterList)
+    }
+  }
+
+
+  def moveToSuppliedFolder(origPath: String, list: List[File]): Unit = list.map { x =>
+    x.renameTo(getSrcAndDest(origPath, x))
+  }
+
+  def getSrcAndDest(origPath: String, file: File) = {
+    new File(s"${origPath}/${file.getName}")
+  }
+
+  def filterByFileType(fileType: String, list: List[File]): List[File] = {
+    list.filter(x => x.getName.takeRight(3) == fileType)
   }
 
   def reduceFoldersToList(path: String): List[File] =  {
@@ -50,9 +76,4 @@ object ReadFiles {
       }
     }
   }
-
-
-
-
-
 }
